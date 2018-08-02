6d3df21b4ee1841d036670e295cf79dd074b192d
felipechaux <felipechauxx@misena.edu.co>
2018-08-02 17:00:20 -0500

    validaciones chatbot

--- MiBot/app/src/main/java/com/code/chaux/mibot/MainActivity.kt
+++ MiBot/app/src/main/java/com/code/chaux/mibot/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.code.chaux.mibot
 
 import android.app.Activity
-import android.graphics.Bitmap
+import android.content.Context
 import android.graphics.BitmapFactory
-import android.net.Uri
-import android.os.AsyncTask
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
 import com.github.bassaer.chatmessageview.model.ChatUser
 import com.github.bassaer.chatmessageview.model.Message
@@ -16,21 +15,20 @@ import com.github.kittinunf.fuel.core.FuelManager
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import  android.util.Log;
-import android.text.Html
-import java.net.URL
+import android.widget.Toast
 
 
-class MainActivity : Activity()  {
+class MainActivity : Activity() {
 
     companion object {
         private const val ACCESS_TOKEN = "7a13facd828c41f181af2f415899a98d"
     }
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         FuelManager.instance.baseHeaders = mapOf(
                 "Authorization" to "Bearer $ACCESS_TOKEN"
@@ -47,89 +45,122 @@ class MainActivity : Activity()  {
 
         val human = ChatUser(
                 1,
-                "Yo",
+                "Tu",
                 BitmapFactory.decodeResource(resources,
                         R.drawable.alien)
         )
 
         val agent = ChatUser(
                 2,
-                "Agente",
+                "Musicalin",
                 BitmapFactory.decodeResource(resources,
-                        R.drawable.chaticon)
+                        R.drawable.musicalin)
         )
 
-        my_chat_view.setOnClickSendButtonListener(View.OnClickListener {
+        if(isNetworkAvailable()){
+            agentResponse(agent, "hola")
+        }else{
+
             my_chat_view.send(Message.Builder()
-                    .setUser(human)
-                    .setText(my_chat_view.inputText)
+                    .setUser(agent)
+                    .setText(applicationContext.getString(R.string.no_conexion_internet))
                     .build()
             )
+           // Toast.makeText(applicationContext, R.string.no_conexion_internet, Toast.LENGTH_LONG).show()
+        }
 
-            Fuel.get("/query",
-                    listOf("query" to my_chat_view.inputText))
-                    .responseJson { _, _, result ->
-                        val reply = result.get().obj()
-                                .getJSONObject("result")
-                                .getJSONObject("fulfillment")
-                                .getString("speech")
+        my_chat_view.setOnClickSendButtonListener(View.OnClickListener {
 
 
+            if (my_chat_view.inputText != null && !my_chat_view.inputText.equals("")) {
 
-                        val picture = BitmapFactory.decodeResource(resources, R.drawable.giphy)
+                if(isNetworkAvailable()){
+                    my_chat_view.send(Message.Builder()
+                            .setUser(human)
+                            .setText(my_chat_view.inputText)
+                            .build()
+                    )
+                    agentResponse(agent, my_chat_view.inputText)
 
-                        Log.e("tag","message prueba json "+result.get().content)
-                        Log.e("tag","message prueba json22 ")
+                }else{
+                    my_chat_view.send(Message.Builder()
+                            .setUser(agent)
+                            .setText(applicationContext.getString(R.string.no_conexion_internet))
+                            .build()
+                    )
+                }
 
-                        if(result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getString("messages").contains("image")){
 
-                            Log.e("tag","si contiene url image y es : "+result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getJSONArray("messages"))
-
-                            val url=result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getJSONArray("messages").getJSONObject(0).getJSONObject("image").getString("url")
-
-                            val textImage=result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getJSONArray("messages").getJSONObject(0).getString("formattedText")
-
-                            Log.e("tag","texto imagen --> "+textImage)
-
-                            Log.e("tag","URL --> "+url)
-
-                            my_chat_view.send(Message.Builder()
-                                    .setRight(true)
-                                    .setUser(agent)
-                                    .setText(textImage)
-                                    .build()
-                            )
-                            my_chat_view.send(Message.Builder()
-                                    .setRight(true)
-                                    .setUser(agent)
-                                    .setPicture(DownLoadImageTask(url).execute().get())
-                                    .setType(Message.Type.PICTURE)
-                                    .build()
-
-                            )
-
-                        }
-
-                        else
-                        {
-
-                            my_chat_view.send(Message.Builder()
-                                    .setRight(true)
-                                    .setUser(agent)
-                                    .setText(reply)
-                                    .build()
-                            )
-
-                            Log.e("tag","NO contiene url image ")
-                        }
-                    }
+            }
 
 
         })
 
     }
 
+    fun agentResponse(agent: ChatUser, dialogo: String): Unit {
 
+        Fuel.get("/query",
+                listOf("query" to dialogo))
+                .responseJson { _, _, result ->
+                    val reply = result.get().obj()
+                            .getJSONObject("result")
+                            .getJSONObject("fulfillment")
+                            .getString("speech")
+
+                    Log.e("tag", "message  json " + result.get().content)
+
+                    if (result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getString("messages").contains("image")) {
+
+                        Log.e("tag", "si contiene url image y es : " + result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getJSONArray("messages"))
+
+                        val url = result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getJSONArray("messages").getJSONObject(0).getJSONObject("image").getString("url")
+
+                        val textImage = result.get().obj().getJSONObject("result").getJSONObject("fulfillment").getJSONArray("messages").getJSONObject(0).getString("formattedText")
+
+                        Log.e("tag", "texto imagen --> " + textImage)
+
+                        Log.e("tag", "URL --> " + url)
+
+                        my_chat_view.send(Message.Builder()
+                                .setRight(true)
+                                .setUser(agent)
+                                .setText(textImage)
+                                .build()
+                        )
+                        my_chat_view.send(Message.Builder()
+                                .setRight(true)
+                                .setUser(agent)
+                                .setPicture(DownLoadImageTask(url).execute().get())
+                                .setType(Message.Type.PICTURE)
+                                .build()
+
+                        )
+
+                    } else {
+
+                        my_chat_view.send(Message.Builder()
+                                .setRight(true)
+                                .setUser(agent)
+                                .setText(reply)
+                                .build()
+                        )
+
+                        Log.e("tag", "NO contiene url image ")
+                    }
+                }
+
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+        } else false
+
+    }
 
 
 }
